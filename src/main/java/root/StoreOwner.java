2d251a8e367ce81b2910397dc6e6d559664552b7
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2020-03-30 12:27:55 +0200

    Changed private attributes naming conventions to allow for Intellij constructor/setter/getter generation

--- src/main/java/root/StoreOwner.java
+++ src/main/java/root/StoreOwner.java
@@ -1,33 +1,36 @@
 package root;
 
 public class StoreOwner {
-    private UserInfo _userInfo;
-    private String _socialID;
+    private UserInfo userInfo;
+    private String socialID;
 
     StoreOwner(String username, String email, String password, String name, String socialID){
-        _userInfo = new UserInfo();
-        _userInfo.username = username;
-        _userInfo.email = email;
-        _userInfo.password = password;
-        _userInfo.name = name;
-
-        _socialID = socialID;
+        this.userInfo = new UserInfo(username, email, password, name);
+        this.socialID = socialID;
     }
 
     StoreOwner(int id, String username, String email, String password, String name, String socialID){
-        _userInfo = new UserInfo();
-        _userInfo.id = id;
-        _userInfo.username = username;
-        _userInfo.email = email;
-        _userInfo.password = password;
-        _userInfo.name = name;
-
-        _socialID = socialID;
+        this.userInfo = new UserInfo(id, username, email, password, name);
+        this.socialID = socialID;
     }
 
-    StoreOwner(UserInfo userInfo, String _socialID){
-        _userInfo = userInfo;
-        _socialID = _socialID;
+    StoreOwner(UserInfo userInfo, String socialID){
+        this.userInfo = userInfo;
+        this.socialID = socialID;
     }
 
+    public String getSocialID(){
+        return this.socialID;
+    }
+    public void setSocialID(String socialID){
+        this.socialID = socialID;
+    }
+
+    public UserInfo getUserInfo(){
+        return this.userInfo;
+    }
+
+    public void setUserInfo(UserInfo userInfo) {
+        this.userInfo = userInfo;
+    }
 }
