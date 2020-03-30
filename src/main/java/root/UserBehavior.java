0debf08077687d2754e1981ab15a8fc712756207
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2020-03-30 15:03:44 +0200

    Stubbing out UserBehavior class

---
+++ src/main/java/root/UserBehavior.java
@@ -0,0 +1,9 @@
+package root;
+
+public abstract class UserBehavior{
+    public abstract void register(RegistrationHolder regHolder);
+
+    public String hash(String s) throws Exception{ // not yet implemented
+        throw new UnsupportedOperationException();
+    }
+}
