c5db5160530d90e946b6ebd68aa00a99b2849ac2
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2020-04-01 19:56:58 +0200

    Modified register function to handle Verifier exceptions

--- src/main/java/root/StoreOwnerController.java
+++ src/main/java/root/StoreOwnerController.java
@@ -10,16 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class StoreOwnerController {
 
     @PostMapping("/register")
-    public String register(@RequestBody StoreOwner storeOwner) {
+    public String register(@RequestBody StoreOwner storeOwner){
         StoreOwnerVerifier verifier = new StoreOwnerVerifier();
-        String status = verifier.verify(storeOwner);
-        if(status.equals("OK")){
-            try{
+        String status = "";
+        try{
+            status = verifier.verify(storeOwner);
+            if(status.equals("OK")){
                 StoreOwnerMapper.insert(storeOwner);
-            }catch (Exception e){e.printStackTrace();}
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            status = "Error: Unhandled Exception";
         }
         return status;
     }
-
-
 }
