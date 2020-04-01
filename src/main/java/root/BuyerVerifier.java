7aa687b4d43ca51720647ff95ba7527e8fa004e8
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2020-04-01 19:33:22 +0200

    Added usernameExists function to BuyerVerifier

--- src/main/java/root/BuyerVerifier.java
+++ src/main/java/root/BuyerVerifier.java
@@ -1,5 +1,7 @@
 package root;
 
+import java.sql.SQLException;
+
 public class BuyerVerifier {
     private Verifier verifier;
 
@@ -14,12 +16,18 @@ public class BuyerVerifier {
         return "OK";
     }
 
-    public String verify(Buyer buyer){
-        String status = verifier.verifyUserInfo(buyer.getUserInfo());
-        if(!status.equals("OK"))
-            return  status;
+    private Boolean usernameExists(String username) throws SQLException {
+        return BuyerMapper.select(username) != null;
+    }
 
-        status = verifyAddress(buyer.getAddress());
-        return status;
+    public String verify(Buyer buyer) throws SQLException {
+        String status = verifier.verifyUserInfo(buyer.getUserInfo());
+        if(!status.equals("OK")){
+            return  status;
+        }
+        if(usernameExists(buyer.getUsername())){
+            return "Error: Username already exists";
+        }
+        return verifyAddress(buyer.getAddress());
     }
 }
