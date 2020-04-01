9544f3eb055bbe8460108dbf965d239540104b8a
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2020-04-01 15:35:13 +0200

    Added new schema

--- database/schema.sql
+++ database/schema.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 30, 2020 at 02:31 AM
+-- Generation Time: Apr 01, 2020 at 03:31 PM
 -- Server version: 5.7.17
--- PHP Version: 5.6.30
+-- PHP Version: 7.1.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `fatty_inc`
+-- Database: `online_store`
 --
 
 -- --------------------------------------------------------
@@ -29,12 +29,39 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `admin` (
-  `id` int(10) UNSIGNED NOT NULL,
+  `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
-  `name` varchar(50) NOT NULL
+  `name` varchar(50) NOT NULL,
+  `key_id` int(11) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `email`, `password`, `name`, `key_id`) VALUES
+(3, 'Admin', 'rashad@protonmail.com', '321321321321321', 'Mahmoud Rashad', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_keys`
+--
+
+CREATE TABLE `admin_keys` (
+  `id` int(11) NOT NULL,
+  `key_value` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin_keys`
+--
+
+INSERT INTO `admin_keys` (`id`, `key_value`) VALUES
+(1, 'V2$H9sjbf#K'),
+(2, 'vW8,.@unCi>N');
 
 -- --------------------------------------------------------
 
@@ -51,13 +78,20 @@ CREATE TABLE `buyer` (
   `address` varchar(50) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `buyer`
+--
+
+INSERT INTO `buyer` (`id`, `username`, `email`, `password`, `name`, `address`) VALUES
+(1, 'ahmadddd', 'rashad@protonmail.com', '321321321321321', 'dasas', 'some address test');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `shop_owner`
+-- Table structure for table `store_owner`
 --
 
-CREATE TABLE `shop_owner` (
+CREATE TABLE `store_owner` (
   `id` int(10) UNSIGNED NOT NULL,
   `username` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
@@ -65,6 +99,14 @@ CREATE TABLE `shop_owner` (
   `name` varchar(50) NOT NULL,
   `social_id` varchar(50) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `store_owner`
+--
+
+INSERT INTO `store_owner` (`id`, `username`, `email`, `password`, `name`, `social_id`) VALUES
+(1, 'Ahmad', 'kh-ahmaddsadsa@protonmail.com', 'passwdasdsaord', 'Ahmad', 'social_id123123'),
+(2, 'ahmadddd', 'rashad@protonmail.com', '321321321321321', 'Ahmad', 'social_id123123');
 
 --
 -- Indexes for dumped tables
@@ -76,7 +118,15 @@ CREATE TABLE `shop_owner` (
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`),
-  ADD UNIQUE KEY `email` (`email`);
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `FK_key_id` (`key_id`);
+
+--
+-- Indexes for table `admin_keys`
+--
+ALTER TABLE `admin_keys`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `key_value` (`key_value`);
 
 --
 -- Indexes for table `buyer`
@@ -87,9 +137,9 @@ ALTER TABLE `buyer`
   ADD UNIQUE KEY `email` (`email`);
 
 --
--- Indexes for table `shop_owner`
+-- Indexes for table `store_owner`
 --
-ALTER TABLE `shop_owner`
+ALTER TABLE `store_owner`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`),
   ADD UNIQUE KEY `email` (`email`);
@@ -99,15 +149,25 @@ ALTER TABLE `shop_owner`
 --
 
 --
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `admin_keys`
+--
+ALTER TABLE `admin_keys`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
 -- AUTO_INCREMENT for table `buyer`
 --
 ALTER TABLE `buyer`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
--- AUTO_INCREMENT for table `shop_owner`
+-- AUTO_INCREMENT for table `store_owner`
 --
-ALTER TABLE `shop_owner`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;COMMIT;
+ALTER TABLE `store_owner`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
