098846284b571a1e14237a8b1688621f2d915446
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2020-04-17 19:01:21 +0200

    added token validation

---
+++ src/main/java/root/Security/Filters/ValidJwtTokenFilter.java
@@ -0,0 +1,55 @@
+package root.Security.Filters;
+
+
+import io.jsonwebtoken.Jwt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import root.Security.JwtUtil;
+import root.Security.UserCredentials;
+import root.User.UserType;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class ValidJwtTokenFilter extends OncePerRequestFilter {
+
+
+    public ValidJwtTokenFilter() {
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+
+        System.out.println(authHeader);
+        String username = null;
+        String jwtToken = null;
+        JwtUtil jwtUtil = new JwtUtil();
+        if (authHeader != null && authHeader.startsWith("Bearer "))
+        {
+            jwtToken = authHeader.substring(7);
+            System.out.println(jwtToken);
+            username = jwtUtil.extractUsername(jwtToken);
+        }
+        if (jwtToken == null || !jwtUtil.validateToken(jwtToken, new UserCredentials(username, "", UserType.ADMIN)))
+            throw new ServletException("Bad Credentials");
+        System.out.println(username);
+        filterChain.doFilter(request, response);
+    }
+
+    @Bean
+    public FilterRegistrationBean<ValidJwtTokenFilter> ValidJwtTokenFilterBean() {
+        FilterRegistrationBean<ValidJwtTokenFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new ValidJwtTokenFilter());
+        registrationBean.addUrlPatterns("/user/*");
+        return registrationBean;
+    }
+
+}
--- src/main/java/root/User/Admin/AdminController.java
+++ src/main/java/root/User/Admin/AdminController.java
@@ -4,10 +4,30 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import root.User.Buyer.BuyerMapper;
+import root.User.StoreOwner.StoreOwnerMapper;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
+
+    @RequestMapping("/users")
+    public List<Object> getAll() {
+        List<Object> list = new ArrayList<>();
+        try {
+            list.addAll(StoreOwnerMapper.selectAll());
+            list.addAll(BuyerMapper.selectAll());
+            list.addAll(AdminMapper.selectAll());
+            return list;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
     @PostMapping("/register")
     public String register(@RequestBody Admin admin){
