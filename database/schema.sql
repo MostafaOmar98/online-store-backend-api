9bd9d64093938ed220750694d3549d98c3ebf45b
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2020-04-18 11:59:35 +0200

    Updated schema to work with long passwords

--- database/schema.sql
+++ database/schema.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 01, 2020 at 03:31 PM
+-- Generation Time: Apr 18, 2020 at 11:59 AM
 -- Server version: 5.7.17
--- PHP Version: 7.1.3
+-- PHP Version: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -29,12 +29,12 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `admin` (
-  `id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `key_id` int(11) DEFAULT NULL
+                         `id` int(11) NOT NULL,
+                         `username` varchar(50) NOT NULL,
+                         `email` varchar(50) NOT NULL,
+                         `password` varchar(256) NOT NULL,
+                         `name` varchar(50) NOT NULL,
+                         `key_id` int(11) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -42,7 +42,9 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `username`, `email`, `password`, `name`, `key_id`) VALUES
-(3, 'Admin', 'rashad@protonmail.com', '321321321321321', 'Mahmoud Rashad', 1);
+(1, 'admin', 'admin@protonmail.com', '$2a$10$Zz/A5hPdoz0JRlPtLnirV.LJKdWP8PEUhipLt2DR3Q1v6gn.jjxLC', 'Ahmad', 1),
+(2, 'admin2', 'admi2n@protonmail.com', '$2a$10$m19oKl/YNFPc/KOSdbpm/eduQFBkCgLipK5dNHAHD9qVJrZzp1sNu', 'Ahmad', 3),
+(3, 'admin3', 'buyer2@protonmail.com', '$2a$10$5lIdq2QSXgCn4xTvLYTyVeDLceBjiL0phVDi8iEa/.CbKHBPnLOoe', 'Ahmad', 4);
 
 -- --------------------------------------------------------
 
@@ -51,8 +53,8 @@ INSERT INTO `admin` (`id`, `username`, `email`, `password`, `name`, `key_id`) VA
 --
 
 CREATE TABLE `admin_keys` (
-  `id` int(11) NOT NULL,
-  `key_value` varchar(255) NOT NULL
+                              `id` int(11) NOT NULL,
+                              `key_value` varchar(255) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -61,7 +63,9 @@ CREATE TABLE `admin_keys` (
 
 INSERT INTO `admin_keys` (`id`, `key_value`) VALUES
 (1, 'V2$H9sjbf#K'),
-(2, 'vW8,.@unCi>N');
+(2, 'vW8,.@unCi>N'),
+(3, 'vWaQweR'),
+(4, 'vWeRQASD');
 
 -- --------------------------------------------------------
 
@@ -70,12 +74,12 @@ INSERT INTO `admin_keys` (`id`, `key_value`) VALUES
 --
 
 CREATE TABLE `buyer` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `address` varchar(50) NOT NULL
+                         `id` int(10) UNSIGNED NOT NULL,
+                         `username` varchar(50) NOT NULL,
+                         `email` varchar(50) NOT NULL,
+                         `password` varchar(256) NOT NULL,
+                         `name` varchar(50) NOT NULL,
+                         `address` varchar(50) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -83,7 +87,11 @@ CREATE TABLE `buyer` (
 --
 
 INSERT INTO `buyer` (`id`, `username`, `email`, `password`, `name`, `address`) VALUES
-(1, 'ahmadddd', 'rashad@protonmail.com', '321321321321321', 'dasas', 'some address test');
+(1, 'ahmad', 'ahmad@protonmail.com', '$2a$10$v7ZJpnFkn47YDKzD51w0N.CFUg2jPtnAuPfdBLkhRGU8B3/zSMpSG', 'Ahmad', '2B'),
+(2, 'ahmad2', 'ahmad2@protonmail.com', '$2a$10$CLPfkZZyobhkDTkTr1lGkuRePQsrfiBNS2k6CvQtYMXb9lSIHpT..', 'Ahmad', '2B'),
+(3, 'ahmad3', 'ahmad3@protonmail.com', '$2a$10$QomX65YylJRO5/wpO/XdRucjuaqzf8yNyeS9maMo1VAgd1zmn.bNC', 'Ahmad', '2B'),
+(4, 'buyer', 'buyer@protonmail.com', '$2a$10$a8pwWNlaVX1LHb2phViFLuuP/qZWHSvy1alftsQqnNyuxxwDkS4DO', 'Ahmad', '2B'),
+(5, 'buyer2', 'buyer2@protonmail.com', '$2a$10$81byW1JNEXuvQV3hfM.FZOfD/2fwr.MCuxE1Uqz3CdmGm/4u9TME.', 'Ahmad', '2B');
 
 -- --------------------------------------------------------
 
@@ -92,12 +100,12 @@ INSERT INTO `buyer` (`id`, `username`, `email`, `password`, `name`, `address`) V
 --
 
 CREATE TABLE `store_owner` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `social_id` varchar(50) NOT NULL
+                               `id` int(10) UNSIGNED NOT NULL,
+                               `username` varchar(50) NOT NULL,
+                               `email` varchar(50) NOT NULL,
+                               `password` varchar(256) NOT NULL,
+                               `name` varchar(50) NOT NULL,
+                               `social_id` varchar(50) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -105,8 +113,8 @@ CREATE TABLE `store_owner` (
 --
 
 INSERT INTO `store_owner` (`id`, `username`, `email`, `password`, `name`, `social_id`) VALUES
-(1, 'Ahmad', 'kh-ahmaddsadsa@protonmail.com', 'passwdasdsaord', 'Ahmad', 'social_id123123'),
-(2, 'ahmadddd', 'rashad@protonmail.com', '321321321321321', 'Ahmad', 'social_id123123');
+(1, 'ahmad', 'ahmad@protonmail.com', '$2a$10$FFkn0Hg0QLDFL4vf/fXnPejGtRd2.EFxqfdpz00LOkFhKB4KoizkS', 'Ahmad', '2B'),
+(2, 'ahmad2', 'ahma2d@protonmail.com', '$2a$10$ORTKDXF7yOrqLk0r1Z04Z.cayTCNTNmSZ9mj0bF6tyGtrzuNKVBwe', 'Ahmad', '2B');
 
 --
 -- Indexes for dumped tables
@@ -116,33 +124,33 @@ INSERT INTO `store_owner` (`id`, `username`, `email`, `password`, `name`, `socia
 -- Indexes for table `admin`
 --
 ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD KEY `FK_key_id` (`key_id`);
+    ADD PRIMARY KEY (`id`),
+    ADD UNIQUE KEY `username` (`username`),
+    ADD UNIQUE KEY `email` (`email`),
+    ADD KEY `FK_key_id` (`key_id`);
 
 --
 -- Indexes for table `admin_keys`
 --
 ALTER TABLE `admin_keys`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `key_value` (`key_value`);
+    ADD PRIMARY KEY (`id`),
+    ADD UNIQUE KEY `key_value` (`key_value`);
 
 --
 -- Indexes for table `buyer`
 --
 ALTER TABLE `buyer`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`),
-  ADD UNIQUE KEY `email` (`email`);
+    ADD PRIMARY KEY (`id`),
+    ADD UNIQUE KEY `username` (`username`),
+    ADD UNIQUE KEY `email` (`email`);
 
 --
 -- Indexes for table `store_owner`
 --
 ALTER TABLE `store_owner`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`),
-  ADD UNIQUE KEY `email` (`email`);
+    ADD PRIMARY KEY (`id`),
+    ADD UNIQUE KEY `username` (`username`),
+    ADD UNIQUE KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -152,22 +160,22 @@ ALTER TABLE `store_owner`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `admin_keys`
 --
 ALTER TABLE `admin_keys`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `buyer`
 --
 ALTER TABLE `buyer`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+    MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `store_owner`
 --
 ALTER TABLE `store_owner`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;COMMIT;
+    MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
